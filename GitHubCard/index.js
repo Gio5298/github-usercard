625075d1b567b0fa65bd0d994f46a6a7ec75d63b
Regino P <regino5298@gmail.com>
2019-10-31 16:04:16 -0700

    added other cards, content and classes

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,14 +3,70 @@
            https://api.github.com/users/<your name>
 */
 
-axios.get('https://api.github.com/users/Gio5298')
-.then((response) => {
-  console.log(response);
-})
-.catch((error) => {
-  console.log(error);
-})
+// axios.get('https://api.github.com/users/Gio5298')
+// .then((response) => {
+//   console.log(response);
+// })
+// .catch((error) => {
+//   console.log(error);
+// })
 
+//My own card vvv
+axios.get('https://api.github.com/users/Gio5298')
+.then(response => {
+  	console.log(response.data)
+    cardMaker(response.data)
+  })
+    .catch(error => {
+    console.log(error)
+  });
+
+  //Others Cards vvv
+  axios.get('https://api.github.com/users/leachcoding')
+.then(response => {
+  	console.log(response.data)
+    cardMaker(response.data)
+  })
+    .catch(error => {
+    console.log(error)
+  });
+
+  axios.get('https://api.github.com/users/MarFan')
+.then(response => {
+  	console.log(response.data)
+    cardMaker(response.data)
+  })
+    .catch(error => {
+    console.log(error)
+  });
+
+  axios.get('https://api.github.com/users/bseverino')
+.then(response => {
+  	console.log(response.data)
+    cardMaker(response.data)
+  })
+    .catch(error => {
+    console.log(error)
+  });
+
+  axios.get('https://api.github.com/users/crutledgedev')
+.then(response => {
+  	console.log(response.data)
+    cardMaker(response.data)
+  })
+    .catch(error => {
+    console.log(error)
+  });
+
+  axios.get('https://api.github.com/users/itshui3')
+  .then(response => {
+      console.log(response.data)
+      cardMaker(response.data)
+    })
+      .catch(error => {
+      console.log(error)
+    });
+  
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -20,17 +76,50 @@ axios.get('https://api.github.com/users/Gio5298')
 
 function cardMaker(profileInfo){
 
-  //create elements
-    const newCard = document.createElement('div');
-    const cardPic = document.createElement('img');
-    const cardContent = document.createElement('div');
-    const cardName = document.createElement('h3');
-    const cardUserName = document.createElement('p');
-    const cardLoc = document.createElement('p');
-    const cardLink = document.createElement('p');
-    const cardFollowers = document.createElement('p');
-    const cardFollowing = document.createElement('p');
-    const cardBio = document.createElement('p');
+  //created elements vvv
+    const newCard = document.createElement('div'),
+      cardPic = document.createElement('img'),
+      cardContent = document.createElement('div'),
+      cardName = document.createElement('h3'),
+      cardUserName = document.createElement('p'),
+      cardLoc = document.createElement('p'),
+      cardLink = document.createElement('p'),
+      cardFollowers = document.createElement('p'),
+      cardFollowing = document.createElement('p'),
+      cardBio = document.createElement('p');
+
+  //created structure vvv
+    newCard.appendChild(cardPic);
+    newCard.appendChild(cardContent);
+    cardContent.appendChild(cardName);
+    cardContent.appendChild(cardUserName);
+    cardContent.appendChild(cardLoc);
+    cardContent.appendChild(cardLink);
+    cardContent.appendChild(cardFollowers);
+    cardContent.appendChild(cardFollowing);
+    cardContent.appendChild(cardBio);
+
+  //set content vvv
+    cardPic.setAttribute ('src', profileInfo.avatar_url)
+    cardName.textContent = profileInfo.name
+    cardUserName.textContent = profileInfo.login
+    cardLoc.textContent = `Location: ${profileInfo.location}`
+    cardPic.setAttribute('alt', `${profileInfo.name}'s photo`)
+    cardLink.setAttribute('href', profileInfo.html_url)
+    cardLink.textContent = `Profile: ${profileInfo.html_url}`
+    cardFollowers.textContent = `Followers: ${profileInfo.followers}`
+    cardFollowing.textContent = `Following: ${profileInfo.following}`
+    cardBio.textContent =  `Bio: ${profileInfo.bio}`
+
+  //applied classes vvv
+    newCard.classList.add('card');
+    cardContent.classList.add('card-info');
+    cardName.classList.add('name');
+    cardUserName.classList.add('username');
+
+  //newCard vvv
+    const cards = document.querySelector('.cards')
+    cards.appendChild(newCard)
 }
 
 /* Step 4: Pass the data received from Github into your function, 
@@ -47,7 +136,7 @@ function cardMaker(profileInfo){
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['https://github.com/leachcoding','https://github.com/itshui3','https://github.com/JacobWashburn','https://github.com/reeceap124','https://github.com/bseverino','https://github.com/crutledgedev','https://github.com/MarFan'];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
